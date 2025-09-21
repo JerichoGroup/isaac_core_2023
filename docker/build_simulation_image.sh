f49cd2922739f4dcf8c49fcd1e295f9f6d0c7b29
Mr-Blum <master420shifu420@tutamail.com>
2025-09-21 15:54:27 +0300

    working in docker, wip abs path's

--- docker/build_simulation_image.sh
+++ docker/build_simulation_image.sh
@@ -5,7 +5,7 @@ set -e
 IMAGE_NAME="isaacsim_2023_ros_humble:core_simulation"
 CONTAINER_NAME="isaacsim_2023_ros_humble_core_simulation"
 COMPOSE_FILE="./docker/simulation_docker/docker-compose.yml"
-LOG_MSG="rclpy loaded"
+LOG_MSG="RTX ready"
 
 echo "Building simulation image..."
 docker compose -f $COMPOSE_FILE build
--- docker/simulation_docker/run_sim_in_docker.sh
+++ docker/simulation_docker/run_sim_in_docker.sh
@@ -1,34 +1,12 @@
 #!/usr/bin/bash
 
-alias ISAACSIM_PYTHON="/isaac-sim/python.sh"
-alias ISAACSIM="/isaac-sim/python.sh"
-alias ISAAC_PATH="/isaac-sim"
-
 source /opt/ros/humble/setup.bash
 source /root/IsaacSim-ros_workspaces/humble_ws/install/setup.bash;
 
-TOPIC=$1
+USD_PATH="--usd-path $PWD/usd/maps/earth/earth.usda"
+COM="--com-udp"
 
-if [ "$TOPIC" = "-h" ]; then
-    echo "Usage flying_camera.bash topic_odom usd_path"
-    exit 0
-fi
-
-if [ -z "$TOPIC" ]
-    then
-        TOPIC=sim/odom
-fi
-echo $TOPIC
-
-USD_PATH=$2
-
-if [ -z "$USD_PATH" ]
-    then
-        USD_PATH="$PWD/usd/maps/earth/earth.usda"
-fi
-echo $USD_PATH
-
-RUN_SIMULATION="$ISAACSIM_PYTHON ./simulation/core_parser.py --usd-path $USD_PATH --odom $TOPIC"
+RUN_SIMULATION="/isaac-sim/python.sh ./simulation/main_sim.py $USD_PATH $COM"
 
 bash -ic "$RUN_SIMULATION"
 

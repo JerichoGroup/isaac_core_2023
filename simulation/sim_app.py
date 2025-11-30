3906f0f4dc5018d0d1c7bb02b5ca72b181d82ebc
Mr-Blum <master420shifu420@tutamail.com>
2025-11-30 20:23:43 +0200

    with func, to CR

--- simulation/sim_app.py
+++ simulation/sim_app.py
@@ -48,6 +48,7 @@ class Simulation:
         self._set_viewport()
         self._update_laser_sensor()
         self._set_cesium_tilesets_url(consts.TILESETS_HTTP_SERVER_URL)
+        self._update_script_node_paths()
         self._configure_camera()
         self._configure_extensions_ros2()
 
@@ -270,7 +271,29 @@ class Simulation:
         
         for prim in tilesets.GetChildren():
             self._update_tileset_url(prim, url)
+
         
+    def _update_script_node_paths(self) -> None:
+        """"updates all the script nodes for activated flags to absolute paths on current computer"""
+
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        script_nodes_map = {
+            "/Environment/bbox_publisher/BboxPublisher/script_node": os.path.join(base_dir, "script_nodes", "bbox_node.py"),
+            "/Environment/distance_sensor/ActionGraph/laser_depth_node": os.path.join(base_dir, "script_nodes", "sensor_node.py"),
+            "/Environment/SAT/takeSAT/script_node": os.path.join(base_dir, "script_nodes", "sat_node.py")
+        }
+
+        for prim_path, abs_script_path in script_nodes_map.items():
+            prim = None
+
+            try:
+                prim = get_prim_at_path(prim_path)
+                prim.GetAttribute("inputs:scriptPath").Set(abs_script_path)
+                carb.log_info(f"Updated {prim_path} script path → {abs_script_path}")
+
+            except Exception as e:
+                continue
+
 
 # ==================== laser sensor methods
     def _update_laser_sensor(self) -> None:

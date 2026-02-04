01a0e32c0fe44e4c70fc489755bfd07297bc52f7
Mr-Blum <master420shifu420@tutamail.com>
2026-02-04 11:24:19 +0200

    wip

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ with HostIsaacManager(com_udp=True):
     point_sender = OrbitSender(center_lat=32.22481, center_lon=35.25621,
                                radius_m=1000, height_m=1000.0,
                                speed_mps=50, duration_s=100,
-                               pitch_deg=-45.0, roll_deg=0.0)
-    # point_sender.run(blocking=True)
+                               pitch_deg=0.0, roll_deg=0.0)
+    point_sender.run(blocking=True)
     time.sleep(1000)
 
 utils.safe_rclpy_shutdown()
